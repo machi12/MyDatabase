5c60bbb07b956c9ef9899fc16344e86661df6281
machi12 <1224881150@qq.com>
2019-12-23 12:40:02 +0800

    1.1

---
+++ src/Start.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * @ Description   :  自定义数据库的主类
+ * @ Author        :  马驰
+ * @ CreateDate    :  2019/12/21 13:17
+ */
+public class Start {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = null;
+        do{
+            System.out.print(">>");
+            
+        }while();
+    }
+}
