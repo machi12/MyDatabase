097e1511440e96ac8bee1bea47636722770dcbe5
machi12 <1224881150@qq.com>
2020-01-02 21:23:07 +0800

    1.7

--- src/sdnu/machi/Format.java
+++ src/sdnu/machi/Format.java
@@ -14,9 +14,9 @@ public class Format {
         //去掉字符串前后两端的空格
         sql = input.trim();
         //将字符串都转化为小写
-        sql.toLowerCase();
+        String string = sql.toLowerCase();
         //通过正则表达式将多个空格转换为一个空格
-        String str = sql.replaceAll("\\s{2,}", " ");
+        String str = string.replaceAll("\\s{2,}", " ");
         //去掉;前的空格
         String s = str.replaceFirst("( ;)$", ";");
         //System.out.println(s);
--- src/sdnu/machi/Insert.java
+++ src/sdnu/machi/Insert.java
@@ -34,14 +34,18 @@ public class Insert {
 
             //获取insert命令中两个括号中的内容
             List<String> list1 = new ArrayList<>();
-            Pattern pattern = Pattern.compile("\\(.*\\)");
+            Pattern pattern = Pattern.compile("\\(.*?\\)");
             Matcher matcher = pattern.matcher(sql);
             while (matcher.find()) {
                 list1.add(matcher.group());
             }
+//            System.out.println(list1.size());
+//            for(String s:list1){
+//                System.out.println(s);
+//            }
 
             //判断语句是否正确,根据()的数据
-            if (list.size() > 2) {
+            if (list.size() != 2) {
                 System.out.println("ERROR: 语句有错误");
                 Input.get();
             } else {
@@ -49,6 +53,41 @@ public class Insert {
 //                for(String s: columnName){
 //                    System.out.println(s);
 //                }
+
+                String s1 = list1.get(0).substring(1, list1.get(0).length()-1).trim();      //第一个括号,字段
+                String s2 = list1.get(1).substring(1, list1.get(1).length()-1).trim();      //第二个括号,值
+
+                //对字符串进行转义
+                //String s11 = transMean(s1);
+                String s22 = transMean(s2);
+
+                String[] key = s1.split(",");
+                String[] value = s22.split(",");
+
+                //String s = "";      //最终要存储的字符串对象
+
+//                System.out.println(s1);
+//                System.out.println(s2);
+
+
+                String sep = SQLConstant.getSeparate();
+                int len = columnName.size();
+                String s = "";      //最终要存储的字符串对象
+                for(int i = 0; i < len; i++){
+                    String s3 = columnName.get(i);
+                    int flag = 0;
+                    for(int j = 0; j < key.length; j++){
+                        if(key[j].equals(s3)){
+                            s += value[j] + sep;
+                            flag = 1;
+                        }
+                    }
+                    if(flag == 0){
+                        s += "null" + sep;
+                    }
+                }
+                System.out.println(s);
+                Input.get();
             }
         }
         else{
@@ -92,4 +131,18 @@ public class Insert {
 
         return list;
     }
+
+    /**
+     * @Description  : 对传入的字符串进行转义,如果字符串中有分隔符,则需要在分隔符前再加一个分隔符
+     * @author       : 马驰
+     * @param        : str  需要转义的字符串
+     * @return       : String 转义后的字符串
+     */
+    private static String transMean(String str){
+        String sep = SQLConstant.getSeparate();     //分隔符
+        String s = str.replaceAll(sep, sep + sep);
+
+        return s;
+    }
+
 }
